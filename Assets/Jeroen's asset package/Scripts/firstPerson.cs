e10fbb99219f5374a9a899dbc0a05d3208d73efa
TheJeronator <45825757+TheJeronator@users.noreply.github.com>
2020-04-22 04:12:07 +0200

    clicking working
    - New scene added
    Why the fuck did I make it clicking even though the game is played with a controller? smh

---
+++ Assets/Jeroen's asset package/Scripts/firstPerson.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstPerson : MonoBehaviour
+{
+    private void Update()
+    {
+        if (this.isActiveAndEnabled)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            if (hit.transform != null)
+            {
+                print(hit.transform.gameObject.name);
+            }
+        }
+
+        }
+    }
+}
