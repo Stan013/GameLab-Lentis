a47c3290e41c908d69596fbb641215e61f8b8cf8
TheJeronator <45825757+TheJeronator@users.noreply.github.com>
2020-04-29 05:30:46 +0200

    First Person mechanic
    working and implemented into 2 scenes, can be accessed by pressing X on the controller

--- Assets/Jeroen's asset package/Scripts/firstPerson.cs
+++ Assets/Jeroen's asset package/Scripts/firstPerson.cs
@@ -1,34 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class firstPerson : MonoBehaviour
 {
+    public GameObject firstObject;
+    public GameObject secondObject;
+    public GameObject thirdObject;
+    private Camera thisCam;
+    public int selectedObj;
+    public static bool IsLeft, IsRight, IsUp, IsDown;
+    private float _LastX, _LastY;
+
+    private void Start()
+    {
+        thisCam = gameObject.GetComponentInChildren<Camera>();
+        thisCam.enabled = false;
+    }
     private void Update()
     {
-        if (this.isActiveAndEnabled)
+        if (Input.GetButtonDown("activateFirstPerson"))
         {
-            Cursor.lockState = CursorLockMode.Confined;
-            Cursor.visible = true;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out hit, 100.0f))
-        {
-            if (hit.transform != null)
+            if (thisCam.isActiveAndEnabled)
             {
-                print(hit.transform.gameObject.name);
+                thisCam.enabled = false;
+            }
+            else
+            {
+                thisCam.enabled = true;
             }
         }
+        float x = Input.GetAxis("DPad X");
 
+
+        IsLeft = false;
+        IsRight = false;
+
+        if (_LastX != x)
+        {
+            if (x == -1)
+                IsLeft = true;
+            else if (x == 1)
+                IsRight = true;
         }
+        _LastX = x;
+
+        if (IsRight == true && selectedObj < 3)
+        {
+            selectedObj += 1;
+        }
+        if (IsLeft == true && selectedObj > 1)
+        {
+            selectedObj -= 1;
+        }
+        switch (selectedObj)
+        {
+            case 1:
+                firstObject.GetComponent<Renderer>().material.color = Color.green;
+                secondObject.GetComponent<Renderer>().material.color = Color.white;
+                thirdObject.GetComponent<Renderer>().material.color = Color.white;
+                break;
+            case 2:
+                firstObject.GetComponent<Renderer>().material.color = Color.white;
+                secondObject.GetComponent<Renderer>().material.color = Color.green;
+                thirdObject.GetComponent<Renderer>().material.color = Color.white;
+                break;
+            case 3:
+                firstObject.GetComponent<Renderer>().material.color = Color.white;
+                secondObject.GetComponent<Renderer>().material.color = Color.white;
+                thirdObject.GetComponent<Renderer>().material.color = Color.green;
+                break;
+        }
+
     }
 }
