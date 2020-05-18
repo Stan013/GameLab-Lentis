0262cb7aac84c8b6813e709378982c9b587b7588
Stan013 <32732917+Stan013@users.noreply.github.com>
2020-05-18 02:59:45 +0200

    Quest path reverse

---
+++ Assets/Disable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disable : MonoBehaviour
+{
+    void OnTriggerLeave(Collider collider){
+        collider.gameObject.SetActive(false);
+    }
+}
