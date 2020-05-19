7af7450899d9e38a9a4019d6ab3aebeac7d976e8
TheJeronator <45825757+TheJeronator@users.noreply.github.com>
2020-05-19 12:05:41 +0200

    working plate pickup

--- Assets/Jeroen's asset package/Scripts/platePickup.cs
+++ Assets/Jeroen's asset package/Scripts/platePickup.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class platePickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject platesSpawner;
+    public Transform location;
+    public int plateAmount;
+    public GameObject platePrefab;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        plateAmount = platesSpawner.GetComponent<platesSpawning>().amountofPlates;
+        float distance = Vector3.Distance(gameObject.transform.position, platesSpawner.transform.position);
+        if (plateAmount > 0)
+        {
+            Debug.Log("works");
+            if (Input.GetKeyDown(KeyCode.Joystick1Button1) && distance <= 5f)
+                {
+                plateAmount -= 1;
+                GameObject G = Instantiate(platePrefab, location.position, Quaternion.Euler(-90, 0, 0));
+                G.transform.parent = location;
+                G.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
     }
 }
