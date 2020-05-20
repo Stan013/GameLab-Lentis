12501ff19f4dda80819ef6270ffa84ca2cb08395
Stan013 <32732917+Stan013@users.noreply.github.com>
2020-05-20 05:59:02 +0200

    Door animation

---
+++ Assets/Scripts/BackDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackDoor : MonoBehaviour
+{
+    [SerializeField] private Animator animatorLift;
+
+    void Start () {
+        animatorLift.enabled = true;
+        animatorLift.SetBool("Open2", false); //when you have animations in your game, there's a tab next to "Scene", "Game" and "Asset Store" named "Animator". There
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Guest" || collider.gameObject.tag == "Player"){
+            if(animatorLift.GetBool("Open2") == false){
+                animatorLift.SetBool("Open2", true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if(collider.gameObject.tag == "Guest" || collider.gameObject.tag == "Player"){
+            if(animatorLift.GetBool("Open2") == true){
+                animatorLift.SetBool("Open2", false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FrontDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontDoor : MonoBehaviour
+{
+    [SerializeField] private Animator animatorLift;
+
+    void Start () {
+        animatorLift.enabled = true;
+        animatorLift.SetBool("Open", false);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Guest" || collider.gameObject.tag == "Player"){
+            if(animatorLift.GetBool("Open") == false){
+                animatorLift.SetBool("Open", true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if(collider.gameObject.tag == "Guest" || collider.gameObject.tag == "Player"){
+            if(animatorLift.GetBool("Open") == true){
+                animatorLift.SetBool("Open", false);
+            }
+        }
+    }
+}
