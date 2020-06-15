c7a0f290b5a6434ddaf383db35b69853b99ad4ab
TheJeronator <45825757+TheJeronator@users.noreply.github.com>
2020-06-15 05:09:17 +0200

    platespawner finally fixed
    Didn't add removing at the end of phasetimer since the entire phasetimer script is private

--- Assets/Jeroen's asset package/Scripts/platesSpawning.cs
+++ Assets/Jeroen's asset package/Scripts/platesSpawning.cs
@@ -8,15 +8,28 @@ public class platesSpawning : MonoBehaviour
     public Vector3 size;
     public GameObject platePrefab;
     public int amountofPlates = 0;
+    public float timeAMt = 5;
+    float time;
+    public int maxPlayers = 1;
+    public int amountOfPlayers = 0;
+    public List<GameObject> targets = new List<GameObject>();
+    public List<GameObject> plates = new List<GameObject>();
+    public PhaseTimer phasemanager;
+
     public void Start()
     {
         StartCoroutine(spawnPlates());
+    }
+    public void Update()
+    {
+        //score removing script for plates at the end of phases goes here.
     }
     public void plateSpawner()
     {
         Vector3 pos = center + new Vector3(Random.Range(-size.x / 2 , size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
 
-        Instantiate(platePrefab, pos, Quaternion.Euler(-90, 0, 0));
+        GameObject plate = Instantiate(platePrefab, pos, Quaternion.Euler(-90, 0, 0));
+        plates.Add(plate);
     }
     public IEnumerator spawnPlates()
     {
@@ -24,8 +37,72 @@ public class platesSpawning : MonoBehaviour
         {
             yield return new WaitForSeconds(20);
             plateSpawner();
-            amountofPlates += 1;
+            if (amountofPlates < 5)
+            {
+                amountofPlates += 1;
+            }
             spawnPlates();
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && amountOfPlayers == maxPlayers)
+        {
+
+            if (Input.GetButton(other.GetComponent<Movement>().activityButton) && amountofPlates != 0)
+            {
+                if (this.gameObject.GetComponent<Animator>() != null && this.gameObject.GetComponent<Animator>().enabled == true)
+                {
+                    this.gameObject.GetComponent<Animator>().SetBool("Collision", true);
+                }
+                other.GetComponent<Movement>().button.enabled = false;
+                if (time <= timeAMt)
+                {
+                    time += Time.deltaTime;
+                    other.gameObject.GetComponent<Movement>().activitytimer.fillAmount = time / timeAMt;
+                    Debug.Log(time);
+                }
+                if (time >= timeAMt)
+                {
+                    time = 0;
+
+                    amountofPlates -= 1;
+                    GameObject plateToRemove = plates[0];
+                    plates.Remove(plateToRemove);
+                    Destroy(plateToRemove);
+                    ScoreManager.finalScore += 10;
+                }
+            }
+            else
+            {
+                time = 0;
+                other.GetComponent<Movement>().button.enabled = true;
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            targets.Add(other.gameObject);
+            other.GetComponent<Movement>().button.enabled = true;
+            amountOfPlayers += 1;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            targets.Remove(other.gameObject);
+            amountOfPlayers -= 1;
+            time = 0;
+            other.GetComponent<Movement>().activitytimer.fillAmount = time / timeAMt;
+            other.GetComponent<Movement>().button.enabled = false;
+            if (this.gameObject.GetComponent<Animator>() != null && this.gameObject.GetComponent<Animator>().enabled == true)
+            {
+                this.gameObject.GetComponent<Animator>().SetBool("Collision", false);
+            }
+
         }
     }
     void OnDrawGizmosSelected()
